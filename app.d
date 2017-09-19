2a8c996b1b8b77e686a58bc871e93fbbfa8945e7
Kotet <8435623+kotet@users.noreply.github.com>
2017-09-19 09:28:14 +0900

    warshallFloyd

--- app.d
+++ app.d
@@ -3,8 +3,42 @@ import std.array;
 import std.conv;
 import std.stdio;
 import std.string;
+import std.traits;
+import std.typecons;
+
+enum INF(T) = T.max / 2;//longの場合10^^18くらい
 
 void main()
 {
+	
+}
 
+/// graphMatrix[from][to] == cost < INF!T
+T[][] graphMatrix(T = long)(size_t e)
+{
+	auto m = new T[][](e, e);
+	foreach (ref r; m)
+		r[] = INF!T;
+	return m;
+}
+/// デバッグ用。GraphMatrixを見やすい文字列にする
+string fmt(T)(T[][] graph)
+{
+	auto result = "";
+	foreach (r; graph)
+	{
+		result ~= r.map!(n => (n == INF!T) ? "max" : n.to!string).join("\t") ~ "\n";
+	}
+	return result;
+}
+
+/// warshallFloyd[from][to] == shortestCost
+T[][] warshallFloyd(T)(T[][] graph)
+{
+	auto result = graph.dup;
+	foreach (relay; 0 .. graph.length)
+		foreach (from; 0 .. graph.length)
+			foreach (to; 0 .. graph.length)
+				result[from][to] = min(result[from][to], result[from][relay] + result[relay][to]);
+	return result;
 }

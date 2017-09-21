8d45bfe5b93c7989ae419d881c69dab502847281
Kotet <8435623+kotet@users.noreply.github.com>
2017-09-21 10:00:37 +0900

    dijkstra,minIndex,maxIndex

--- app.d
+++ app.d
@@ -7,7 +7,7 @@ import std.string;
 import std.traits;
 import std.typecons;
 
-enum INF(T) = T.max / 2; //longの場合10^^18くらい
+enum INF(T) = T.max / 2; ///longの場合10^^18くらい
 
 void main()
 {
@@ -44,5 +44,51 @@ T[][] warshallFloyd(T)(T[][] graph)
 		foreach (from; 0 .. graph.length)
 			foreach (to; 0 .. graph.length)
 				result[from][to] = min(result[from][to], result[from][relay] + result[relay][to]);
+	return result;
+}
+
+T[] dijkstra(T = long)(T[][] graph, size_t from)
+{
+	auto result = new T[](graph.length);
+	result[] = INF!T;
+	result[from] = 0;
+	auto queue = new bool[](graph.length);
+	queue[] = false;
+	foreach (_; 0 .. graph.length)
+	{
+		size_t min = result.maxIndex();
+		foreach (i; 0 .. graph.length)
+			if (queue[i] == false && result[i] < result[min])
+				min = i;
+		queue[min] = true;
+		writeln(queue, min);
+
+		foreach (to; 0 .. graph.length)
+		{
+			if (result[min] + graph[min][to] < result[to])
+				result[to] = result[min] + graph[min][to];
+		}
+	}
+	return result;
+}
+
+/// dmd-2.070のstd.algorithmにはminIndexがない
+size_t minIndex(T)(T[] array)
+{
+	size_t result;
+	T _n = array[0];
+	foreach (i, n; array)
+		if (n < _n)
+			result = i;
+	return result;
+}
+/// dmd-2.070のstd.algorithmにはmaxIndexがない
+size_t maxIndex(T)(T[] array)
+{
+	size_t result;
+	T _n = array[0];
+	foreach (i, n; array)
+		if (_n < n)
+			result = i;
 	return result;
 }

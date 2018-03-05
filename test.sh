f7fc8f13b7921be59ff752d997eb20b42ee4fe04
Kotet <kotet0418@gmail.com>
2018-03-05 19:30:56 +0900

    作り直し

---
+++ test.sh
@@ -0,0 +1,21 @@
+BUILD="sudo docker run --rm -v $PWD:/src -w /src dlanguage/dmd:2.070.1 dmd app.d"
+
+echo "Building..."
+echo $BUILD
+$BUILD
+
+echo "Removing old output..."
+rm output/*
+
+for file in `ls input`; do
+    echo -n "Testing $file..."
+    ./app < input/$file > output/$file
+    if diff -q output/$file answer/$file > /dev/null; then
+        echo "passed."
+        rm output/$file
+    else
+        echo "failed."
+    fi
+done
+
+rm -f app app.o

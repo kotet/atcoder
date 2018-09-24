c20d6fdd34411d9f7ea9f36047bc6301b65d7fe0
Kotet <kotet0418@gmail.com>
2018-09-24 09:26:03 +0900

    Add snippets.d

---
+++ snippets.d
@@ -0,0 +1,20 @@
+/// 素因数分解
+long[long] factorize(long x)
+{
+    assert(0 < x, "x is negative");
+    long[long] ps;
+    while ((x & 1) == 0)
+    {
+        x /= 2;
+        ps[2] = (2 in ps) ? ps[2] + 1 : 1;
+    }
+    for (long i = 3; i * i <= x; i += 2)
+        while (x % i == 0)
+        {
+            x /= i;
+            ps[i] = (i in ps) ? ps[i] + 1 : 1;
+        }
+    if (x != 1)
+        ps[x] = (x in ps) ? ps[x] + 1 : 1;
+    return ps;
+}

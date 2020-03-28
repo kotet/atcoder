93886b490bb8c38dcbc36aa0db6dbeb497283ed0
Kotet <kotet0418@gmail.com>
2020-03-28 20:19:01 +0900

    lreads

--- app.d
+++ app.d
@@ -4,6 +4,7 @@ import core.bitop, std.algorithm, std.ascii, std.bigint, std.conv, std.math,
 
 // dfmt off
 T lread(T = long)(){return readln.chomp.to!T();}
+T[] lreads(T = long)(long n){return generate(()=>readln.chomp.to!T()).take(n).array();}
 T[] aryread(T = long)(){return readln.split.to!(T[])();}
 void scan(TList...)(ref TList Args){auto line = readln.split();
 foreach (i, T; TList){T val = line[i].to!(T);Args[i] = val;}}
